2de110aa2355293e9c66f37868db7881d4693542
chirprush <chirpyboat73@gmail.com>
2021-08-02 21:47:01 -0400

    Added bundle boxes

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -48,4 +48,50 @@ const selectCabinetAmount = (index) => {
 	}
 }
 
+const bundleRemoveChild = (container) => {
+	container.removeChild(container.children[container.children.length - 1]);
+}
+
+const bundleAddChild = (container) => {
+	let node = new DOMParser().parseFromString(`
+		<div class="bundle-box">
+			<span>Bundle Plan</span>
+			<span>Width✕Height: </span>
+			<select id="bundle-single-depth" class="selector-dropdown">
+				<optgroup>
+					<option value="0">1000mm</option>
+					<option value="1">1200mm</option>
+				</optgroup>
+			</select>
+			<select id="bundle-single-height" class="selector-dropdown">
+				<optgroup>
+					<option value="0">Quarter</option>
+					<option value="1">Third</option>
+					<option value="2">Half</option>
+				</optgroup>
+			</select>
+		</div>
+	`, "text/html").body.children[0];
+	container.appendChild(node);
+}
+
+const changeBundleAmount = (n) => {
+	let amount = document.getElementById("selector-bundle-amount");
+	let length = (parseInt(amount.value) || 1) + n;
+	let value = Math.max(length, 1);
+	amount.value = value.toString();
+	let container = document.getElementById("selector-more-bundle-container");
+	let to_add = length - container.children.length;
+	if (to_add < 0) {
+		if (length == 0) { return; }
+		for (let i = 0; i < Math.abs(to_add); ++i) {
+			bundleRemoveChild(container);
+		}
+	} else if (to_add > 0) {
+		for (let i = 0; i < to_add; ++i) {
+			bundleAddChild(container);
+		}
+	}
+}
+
 window.onload = () => {}
